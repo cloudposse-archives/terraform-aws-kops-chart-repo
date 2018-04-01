ea64e1775cc5711bf0caea660356e9fa61d665ea
Andriy Knysh <aknysh@users.noreply.github.com>
2018-04-01 09:44:29 -0400

    Initial implementation (#1)
    * Initial implementation
    
    * Update `README`

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Name of the Kops DNS zone (e.g. `domain.com`)"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map('BusinessUnit`,`XYZ`)"
+}
+
+variable "nodes_name" {
+  type        = "string"
+  default     = "nodes"
+  description = "k8s nodes subdomain name in the Kops DNS zone"
+}
