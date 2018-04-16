9b199b4a3919d4f1d88e13b990121456fa60577e
Andriy Knysh <aknysh@users.noreply.github.com>
2018-04-15 22:29:10 -0400

    Separate `cluster_name` and `name` variables (#2)

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "label" {
 
 module "kops_metadata" {
   source     = "git::https://github.com/cloudposse/terraform-aws-kops-metadata.git?ref=tags/0.1.1"
-  dns_zone   = "${var.name}"
+  dns_zone   = "${var.cluster_name}"
   nodes_name = "${var.nodes_name}"
 }
 
@@ -54,7 +54,7 @@ data "aws_iam_policy_document" "default" {
 resource "aws_iam_policy" "default" {
   name        = "${module.label.id}"
   policy      = "${data.aws_iam_policy_document.default.json}"
-  description = "Allow k8s nodes to get/put/delete objects from the chart repo S3 bucket"
+  description = "Allow Kops nodes to get/put/delete objects from the chart repo S3 bucket"
 }
 
 data "aws_iam_policy_document" "role_trust" {
@@ -83,7 +83,7 @@ data "aws_iam_policy_document" "role_trust" {
 resource "aws_iam_role" "default" {
   name               = "${module.label.id}"
   assume_role_policy = "${data.aws_iam_policy_document.role_trust.json}"
-  description        = "Allow k8s nodes to get/put/delete objects from the chart repo S3 bucket"
+  description        = "Allow Kops nodes to get/put/delete objects from the chart repo S3 bucket"
 }
 
 resource "aws_iam_role_policy_attachment" "default" {
--- variables.tf
+++ variables.tf
@@ -10,29 +10,35 @@ variable "stage" {
 
 variable "name" {
   type        = "string"
-  description = "Name of the Kops DNS zone (e.g. `domain.com`)"
+  default     = "chart-repo"
+  description = "Name (e.g. `chart-repo`)"
 }
 
 variable "delimiter" {
   type        = "string"
   default     = "-"
-  description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
   type        = "list"
   default     = []
-  description = "Additional attributes (e.g. `policy` or `role`)"
+  description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
   type        = "map"
   default     = {}
-  description = "Additional tags (e.g. map('BusinessUnit`,`XYZ`)"
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "cluster_name" {
+  type        = "string"
+  description = "Kops cluster name (e.g. `us-east-1.cloudposse.com` or `cluster-1.cloudposse.com`)"
 }
 
 variable "nodes_name" {
   type        = "string"
   default     = "nodes"
-  description = "k8s nodes subdomain name in the Kops DNS zone"
+  description = "Kops nodes subdomain name in the cluster DNS zone"
 }
