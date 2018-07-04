69e79152e8e5a78e9253ce009d08993d8e986e07
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-04 16:31:46 +0300

    Migrate to README.yaml format

---
+++ docs/terraform.md
@@ -0,0 +1,30 @@
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| attributes | Additional attributes (e.g. `1`) | list | `<list>` | no |
+| cluster_name | Kops cluster name (e.g. `us-east-1.cloudposse.com` or `cluster-1.cloudposse.com`) | string | - | yes |
+| delimiter | Delimiter to be used between `namespace`, `stage`, `name` and `attributes` | string | `-` | no |
+| masters_name | Kops masters subdomain name in the cluster DNS zone | string | `masters` | no |
+| name | Name (e.g. `chart-repo`) | string | `chart-repo` | no |
+| namespace | Namespace (e.g. `cp` or `cloudposse`) | string | - | yes |
+| nodes_name | Kops nodes subdomain name in the cluster DNS zone | string | `nodes` | no |
+| permitted_nodes | Kops kubernetes nodes that are permitted to assume the role (e.g. 'nodes', 'masters', or 'both') | string | `both` | no |
+| stage | Stage (e.g. `prod`, `dev`, `staging`) | string | - | yes |
+| tags | Additional tags (e.g. map(`BusinessUnit`,`XYZ`) | map | `<map>` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| bucket_arn | S3 bucket ARN |
+| bucket_domain_name | S3 bucket domain name |
+| bucket_id | S3 bucket ID |
+| policy_arn | IAM policy ARN |
+| policy_id | IAM policy ID |
+| policy_name | IAM policy name |
+| role_arn | IAM role ARN |
+| role_name | IAM role name |
+| role_unique_id | IAM role unique ID |
+
--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,44 @@
 output "bucket_domain_name" {
-  value = "${aws_s3_bucket.default.bucket_domain_name}"
+  value       = "${aws_s3_bucket.default.bucket_domain_name}"
+  description = "S3 bucket domain name"
 }
 
 output "bucket_id" {
-  value = "${aws_s3_bucket.default.id}"
+  value       = "${aws_s3_bucket.default.id}"
+  description = "S3 bucket ID"
 }
 
 output "bucket_arn" {
-  value = "${aws_s3_bucket.default.arn}"
+  value       = "${aws_s3_bucket.default.arn}"
+  description = "S3 bucket ARN"
 }
 
 output "role_name" {
-  value = "${aws_iam_role.default.name}"
+  value       = "${aws_iam_role.default.name}"
+  description = "IAM role name"
 }
 
 output "role_unique_id" {
-  value = "${aws_iam_role.default.unique_id}"
+  value       = "${aws_iam_role.default.unique_id}"
+  description = "IAM role unique ID"
 }
 
 output "role_arn" {
-  value = "${aws_iam_role.default.arn}"
+  value       = "${aws_iam_role.default.arn}"
+  description = "IAM role ARN"
 }
 
 output "policy_name" {
-  value = "${aws_iam_policy.default.name}"
+  value       = "${aws_iam_policy.default.name}"
+  description = "IAM policy name"
 }
 
 output "policy_id" {
-  value = "${aws_iam_policy.default.id}"
+  value       = "${aws_iam_policy.default.id}"
+  description = "IAM policy ID"
 }
 
 output "policy_arn" {
-  value = "${aws_iam_policy.default.arn}"
+  value       = "${aws_iam_policy.default.arn}"
+  description = "IAM policy ARN"
 }
