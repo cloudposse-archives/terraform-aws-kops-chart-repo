13aaf8e5c4509bd018b9a640ef59abd270f28759
rverma-nikiai <38323616+rverma-nikiai@users.noreply.github.com>
2018-12-06 22:14:01 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "bucket_domain_name" {
-  value       = "${aws_s3_bucket.default.*.bucket_domain_name}"
+  value       = "${join("", aws_s3_bucket.default.*.bucket_domain_name)}"
   description = "S3 bucket domain name"
 }
 
 output "bucket_id" {
-  value       = "${aws_s3_bucket.default.*.id}"
+  value       = "${join("", aws_s3_bucket.default.*.id)}"
   description = "S3 bucket ID"
 }
 
 output "bucket_arn" {
-  value       = "${aws_s3_bucket.default.*.arn}"
+  value       = "${join("", aws_s3_bucket.default.*.arn)}"
   description = "S3 bucket ARN"
 }
 
